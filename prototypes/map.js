665cc4e0497de8984bad714de3a0202169692b35
Alison Gale <agale753@gmail.com>
2022-05-30 21:34:42 -0700

    Add group stage games to map

--- prototypes/map.js
+++ prototypes/map.js
@@ -10,10 +10,10 @@ let red = '#FD5211';
 
 let cities = {
     'Adelaide': [138.6007, -34.9285],
-    'Auckland': [174.7645, -36.8509],
+    'Auckland': [174.6645, -36.7509],
     'Brisbane': [153.0260, -27.4705],
     'Dunedin': [170.5006, -45.8795],
-    'Hamilton': [175.2528, -37.7826],
+    'Hamilton': [175.2528, -37.9826],
     'Melbourne': [144.9631, -37.8136],
     'Perth': [115.8613, -31.9523],
     'Sydney': [151.2093, -33.8688],
@@ -28,18 +28,41 @@ let offsets = {
     'Hamilton': [10, 15],
     'Melbourne': [0, 35],
     'Perth': [0, -15],
-    'Sydney': [10, 10],
+    'Sydney': [15, 10],
     'Wellington': [10, 25],
 };
 
+let games;
 let g;
 let projection;
 
-d3.json("data.geojson").then((countries) => {
+Promise.all([d3.json("data.geojson"), d3.csv("games.csv")]).then(([countries, allGames]) => {
+    games = allGames;
     let svg = d3.select("body").append("svg")
         .attr("width", width + 40)
         .attr("height", height);
     g = svg.append("g");
+
+    svg.append("svg:defs").append("svg:marker")
+        .attr("id", "arrow")
+        .attr("viewBox", "0 -4 8 8")
+        .attr('refX', 10)
+        .attr("markerWidth", 4)
+        .attr("markerHeight", 4)
+        .attr("orient", "auto")
+        .attr("fill", green)
+        .append("svg:path")
+        .attr("d", "M0,-4L8,0L0,4");
+    svg.append("svg:defs").append("svg:marker")
+        .attr("id", "arrow2")
+        .attr("viewBox", "0 -4 8 8")
+        .attr('refX', 10)
+        .attr("markerWidth", 4)
+        .attr("markerHeight", 4)
+        .attr("orient", "330")
+        .attr("fill", green)
+        .append("svg:path")
+        .attr("d", "M0,-4L8,0L0,4");
 
     // Render country outlines
     projection = d3.geoMercator().fitSize([width, height], countries);
@@ -57,7 +80,8 @@ d3.json("data.geojson").then((countries) => {
     let dots = g.selectAll("g")
         .data(Object.keys(cities))
         .enter()
-        .append("g");
+        .append("g")
+        .attr("class", "city");
 
     dots.append('circle')
         .attr("cx", (d) => projection(cities[d])[0])
@@ -77,19 +101,16 @@ d3.json("data.geojson").then((countries) => {
 });
 
 function drawPaths() {
+    // Remove the old path if it exists.
+    d3.selectAll("path.line").remove();
+
     let group = document.getElementById('group').value;
     let position = document.getElementById('position').value;
+    let id = `${group}${position}`
 
-    // TODO(agale): Calculate this based on group/position.
-    let destinations = [
-        "Auckland",
-        "Sydney",
-        "Perth",
-        "Wellington",
-        "Adelaide",
-        "Sydney",
-        "Sydney",
-    ];
+    let destinations = games
+        .filter(game => game.home === id || game.away === id)
+        .map(game => game.city);
 
     let index = 0;
     let repeat = () => {
@@ -108,7 +129,7 @@ function drawPaths() {
                     // Find a curve through a point perpendicular to the center point
                     // at a distance of d/6.
                     let d = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2)) * Math.sqrt(10 / 36);
-                    let theta = Math.atan(1 / 3) + Math.atan((y2 - y1) / (x2 - x1));
+                    let theta = Math.atan(1 / 3) + Math.atan(Math.abs((y2 - y1) / (x2 - x1)));
                     let x = Math.cos(theta) * d * Math.sign(x2 - x1);
                     let y = Math.sin(theta) * d * Math.sign(y2 - y1);
                     path.quadraticCurveTo(x1 + x, y1 + y, x2, y2);
@@ -118,21 +139,31 @@ function drawPaths() {
             .attr("stroke-width", 3)
             .attr("stroke", green)
             .attr("opacity", 0.5 + (index / destinations.length) * 0.5)
-            .style("fill", "none");
+            .style("fill", "none")
+            .attr("class", "line");
 
         let totalLength = path.node().getTotalLength();
 
         path.attr("stroke-dasharray", totalLength + " " + totalLength)
             .attr("stroke-dashoffset", totalLength)
             .transition()
-            .duration(500)
+            .duration(1000)
             .ease(d3.easeLinear)
             .attr("stroke-dashoffset", 0)
             .on("end", () => {
-                if (++index < destinations.length) {
+                if (destinations[index] === destinations[index + 1]) {
+                    path.attr("marker-end", "url(#arrow2)");
+                } else {
+                    path.attr("marker-end", "url(#arrow)");
+                }
+                
+                if (++index < destinations.length - 1) {
                     repeat();
                 }
             });
+
+        // Bring the city dots in front of the lines.
+        d3.selectAll('.city').raise();
     };
     repeat();
 }
