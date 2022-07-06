2230d5f42cd5fc62c6abfd5a19d909aa3dd9765b
Alison Gale <agale753@gmail.com>
2022-07-06 07:45:51 -0700

    Render projected finishes

--- web/src/app/schedule/schedule.component.ts
+++ web/src/app/schedule/schedule.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Match, MatchService } from '../match.service';
 
@@ -23,13 +23,32 @@ export class ScheduleComponent implements AfterViewInit {
 
   matches: Match[] = [];
 
-  // TODO(agale): Apply projections to the table.
   projected: Projection[] = [];
 
   readonly allCities = this.matchService.getAllCities();
   readonly groups = this.matchService.getGroups();
 
-  constructor(private readonly matchService: MatchService, readonly countryService: CountryService) {
+  readonly formatMatchMap: { [key: string]: string[] } = {};
+
+  constructor(private readonly matchService: MatchService,
+    readonly countryService: CountryService,
+    private readonly changeDetector: ChangeDetectorRef) {
+    const sortedMatches = this.matchService.getMatches().sort((a, b) => a.id - b.id);
+    const matchesMap: { [key: number]: string[] } = {};
+    for (const match of sortedMatches) {
+      if (match.home.charAt(0) === '1' || match.home.charAt(0) === '2') {
+        matchesMap[match.id] = [match.home, match.away];
+        this.formatMatchMap[match.home] = [match.home];
+        this.formatMatchMap[match.away] = [match.away];
+      } else if (match.home.charAt(0) === 'W') {
+        const home = parseInt(match.home.slice(1));
+        const away = parseInt(match.away.slice(1));
+        matchesMap[match.id] = [...matchesMap[home], ...matchesMap[away]];
+        this.formatMatchMap[match.home] = matchesMap[home];
+        this.formatMatchMap[match.away] = matchesMap[away];
+      }
+    }
+    console.log(this.formatMatchMap);
   }
 
   addProjection() {
@@ -64,6 +83,24 @@ export class ScheduleComponent implements AfterViewInit {
       }
       return cities.some((city: string) => match.city === city);
     });
+    // Need to render the updated matches.
+    this.changeDetector.detectChanges();
+  }
+
+  formatCountry(country: string) {
+    console.log(country);
+    if (country in this.formatMatchMap) {
+      const matched = this.formatMatchMap[country];
+      const matchedProjections =
+        this.projected.filter(p => {
+          return matched.includes(`${p.position}${p.country.charAt(0)}`);
+        });
+      console.log(matchedProjections);
+      if (matchedProjections.length > 0) {
+        return matchedProjections.map(p => this.countryService.formatCountry(p.country)).join('\n');
+      }
+    }
+    return this.countryService.formatCountry(country);
   }
 
   ngAfterViewInit(): void {
